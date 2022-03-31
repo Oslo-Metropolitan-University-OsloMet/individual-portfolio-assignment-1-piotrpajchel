aed4303c56e52f5e7085c22c74043c8106772d16
Piotr Pajchel <piotr.pajchel@gmail.com>
2022-03-31 18:51:50 +0200

    Final commit, added Assigment documentation

---
+++ bot_star.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+prj
+python3 client.py 127.0.0.1 55556 bot Alice &
+python3 client.py 127.0.0.1 55556 bot Dora  &
+python3 client.py 127.0.0.1 55556 bot Chuck  &
+python3 client.py 127.0.0.1 55556 bot Bob  &
